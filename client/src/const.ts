e0e5a3ae505dfb44388ca4717cf85dd56e5fe18b
khemia0101-del <khemia0101@gmail.com>
2026-05-29 11:10:47 -0400

    Delete client/src/const.ts

--- client/src/const.ts
+++
@@ -1,5 +0,0 @@
-export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
-
-export const APP_TITLE = import.meta.env.VITE_APP_TITLE || "Queen Ventures";
-
-export const APP_LOGO = "/images/logo.png";
